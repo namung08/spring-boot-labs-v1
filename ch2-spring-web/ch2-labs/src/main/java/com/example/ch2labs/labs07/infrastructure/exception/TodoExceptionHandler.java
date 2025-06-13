4b819cfb7064b4cc39d240928a53db159b344adc
namung08 <namung08@gmail.com>
2025-06-13 11:52:13 +0900

    :sparkles: feat: TodoExceptionHandler에서 예외 메시지에 HTTP 상태 코드 추가

--- ch2-spring-web/ch2-labs/src/main/java/com/example/ch2labs/labs07/infrastructure/exception/TodoExceptionHandler.java
+++ ch2-spring-web/ch2-labs/src/main/java/com/example/ch2labs/labs07/infrastructure/exception/TodoExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.ch2labs.labs07.infrastructure.exception;
 
 import com.example.ch2labs.labs07.web.dto.common.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,8 +12,8 @@ import java.util.Map;
 @Slf4j
 @ControllerAdvice
 public class TodoExceptionHandler {
-  private Map<String, String> generateMessage(String code, String message) {
-    return Map.of("error_code", code, "message", message);
+  private Map<String, String> generateMessage(HttpStatus status,String code, String message) {
+    return Map.of("status",status.toString(),"error_code", code, "message", message);
   }
 
   @ExceptionHandler(TodoException.class)
@@ -20,7 +21,7 @@ public class TodoExceptionHandler {
     log.error(c.getErrorCode());
     log.error(c.getMessage());
     return ResponseEntity.status(c.getStatus())
-                         .body(CommonResponse.fail(generateMessage(c.getErrorCode(), c.getMessage())));
+                         .body(CommonResponse.fail(generateMessage(c.getStatus(),c.getErrorCode(), c.getMessage())));
   }
 }
 
