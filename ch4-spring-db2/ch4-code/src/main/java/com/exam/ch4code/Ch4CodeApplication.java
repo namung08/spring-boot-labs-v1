58026349e4ab6bc366cf630a9c50c2c187c5d41e
namung08 <namung08@gmail.com>
2025-06-17 12:31:00 +0900

    ✨ feat: Spring Boot 애플리케이션 및 데이터베이스 설정 파일 추가

---
+++ ch4-spring-db2/ch4-code/src/main/java/com/exam/ch4code/Ch4CodeApplication.java
@@ -0,0 +1,13 @@
+package com.exam.ch4code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4CodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch4CodeApplication.class, args);
+  }
+
+}
