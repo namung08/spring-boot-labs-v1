059d224a5cee86e140059d45afc857342a9b7452
namung08 <namung08@gmail.com>
2025-06-17 12:30:23 +0900

    :tada: rocket: Spring Boot 프로젝트 설정 및 기본 파일 추가

---
+++ ch4-spring-db2/ch4-code/src/test/java/com/exam/ch4code/Ch4CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.ch4code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4CodeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
