6d27ab323a8c42c698e17f39d88ea22ce1ad4a55
namung08 <namung08@gmail.com>
2025-06-18 16:28:39 +0900

    ✨ feat: EntityManager를 사용한 JPAQueryFactory 빈 등록 및 QueryDSL 설정 개선

--- ch4-spring-db2/ch4-code/src/main/java/com/exam/ch4code/v3/infrastructure/config/querydsl/QueryDslConfig.java
+++ ch4-spring-db2/ch4-code/src/main/java/com/exam/ch4code/v3/infrastructure/config/querydsl/QueryDslConfig.java
@@ -1,8 +1,20 @@
 package com.exam.ch4code.v3.infrastructure.config.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QueryDslConfig {
+  // spring boot container 가 EntityManager 를 가지고 있음
+  private final EntityManager manager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(manager);
+  }
 
 }
