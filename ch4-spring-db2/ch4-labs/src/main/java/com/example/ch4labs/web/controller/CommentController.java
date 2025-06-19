a673f38b16e84ec2c5d661ef47e1a568e6683dcf
namung08 <namung08@gmail.com>
2025-06-19 15:29:35 +0900

    ✨ feat: CommentController의 API 경로 수정 및 댓글 생성 메서드의 경로 간소화

--- ch4-spring-db2/ch4-labs/src/main/java/com/example/ch4labs/web/controller/CommentController.java
+++ ch4-spring-db2/ch4-labs/src/main/java/com/example/ch4labs/web/controller/CommentController.java
@@ -1,6 +1,5 @@
 package com.example.ch4labs.web.controller;
 
-import com.example.ch4labs.comment.repository.CommentRepository;
 import com.example.ch4labs.comment.sevice.CommentService;
 import com.example.ch4labs.web.dto.comment.request.CommentCreateRequest;
 import com.example.ch4labs.web.dto.comment.response.CommentResponse;
@@ -10,12 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/posts/{postId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
   private final CommentService service;
 
-  @PostMapping("/posts/{postId}/comments")
+  @PostMapping
   public ResponseEntity<CommentResponse> createComment(@PathVariable Long postId, @RequestBody CommentCreateRequest req) {
     return ResponseEntity.status(HttpStatus.CREATED).body(service.createComment(postId, req));
   }
