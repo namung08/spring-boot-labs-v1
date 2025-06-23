31f0620011d60ea82530abebee7e4744a45b943a
namung08 <namung08@gmail.com>
2025-06-23 14:34:17 +0900

    ✨ feat: AuthorizationInterceptor 및 WebConfig 설정 추가 - API 경로에 대한 인증 로직 구현

--- ch5-spring-security/ch5-self/ch5-01-session-based-plain-login/src/main/java/com/captainyun7/ch501sessionbasedplainlogin/config/WebConfig.java
+++ ch5-spring-security/ch5-self/ch5-01-session-based-plain-login/src/main/java/com/captainyun7/ch501sessionbasedplainlogin/config/WebConfig.java
@@ -1,9 +1,20 @@
 package com.captainyun7.ch501sessionbasedplainlogin.config;
 
+import com.captainyun7.ch501sessionbasedplainlogin.interceptor.AuthorizationInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     // TODO
-} 
+ private final AuthorizationInterceptor authorizationInterceptor;
+ @Override
+ public void addInterceptors(InterceptorRegistry registry) {
+  registry.addInterceptor(authorizationInterceptor)
+      .addPathPatterns("/api/**")
+      .excludePathPatterns("/api/auth/**");
+ }
+}
