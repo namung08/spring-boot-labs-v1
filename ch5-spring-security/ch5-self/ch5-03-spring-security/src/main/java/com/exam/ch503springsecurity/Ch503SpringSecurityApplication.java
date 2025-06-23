774c528c025c97220f9feb4fa6c03af2dd0c67f9
namung08 <namung08@gmail.com>
2025-06-23 16:13:18 +0900

    ✨ feat: Spring Boot 프로젝트 초기 설정 및 Gradle 구성 추가 - 기본 파일(.gitattributes, .gitignore, build.gradle, gradlew, gradlew.bat) 및 애플리케이션 구조 생성

---
+++ ch5-spring-security/ch5-self/ch5-03-spring-security/src/main/java/com/exam/ch503springsecurity/Ch503SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.exam.ch503springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch503SpringSecurityApplication {
+
+ public static void main(String[] args) {
+  SpringApplication.run(Ch503SpringSecurityApplication.class, args);
+ }
+
+}
