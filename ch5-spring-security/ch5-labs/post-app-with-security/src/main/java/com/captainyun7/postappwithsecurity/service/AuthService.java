de1fc64d6d2eee5c205f4595a812da512fbd05f6
namung08 <namung08@gmail.com>
2025-06-24 16:01:03 +0900

    ✨ feat: 인증 관련 컨트롤러 및 서비스 클래스 추가 - AuthController와 AuthService 구현

---
+++ ch5-spring-security/ch5-labs/post-app-with-security/src/main/java/com/captainyun7/postappwithsecurity/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.captainyun7.postappwithsecurity.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+}
