7110ae6b6826f9f77d9cf35fd33c057fb4487b83
namung08 <namung08@gmail.com>
2025-06-24 11:09:42 +0900

    ✨ feat: 사용자 세부 정보 로드 기능 구현 - 사용자 이름으로 데이터베이스에서 사용자 정보를 조회하고 UserDetails 객체 반환 로직 추가

--- ch5-spring-security/ch5-self/ch5-03-session-based-spring-security-login/src/main/java/com/captainyun7/ch503sessionbasedspringsecuritylogin/service/UserDetailsServiceImpl.java
+++ ch5-spring-security/ch5-self/ch5-03-session-based-spring-security-login/src/main/java/com/captainyun7/ch503sessionbasedspringsecuritylogin/service/UserDetailsServiceImpl.java
@@ -3,13 +3,14 @@ package com.captainyun7.ch503sessionbasedspringsecuritylogin.service;
 import com.captainyun7.ch503sessionbasedspringsecuritylogin.domain.User;
 import com.captainyun7.ch503sessionbasedspringsecuritylogin.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +21,29 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // TODO: [6] 사용자 이름(username)을 사용하여 데이터베이스에서 사용자 정보를 조회합니다.
-        // TODO: [7] 조회된 사용자 정보가 없으면 UsernameNotFoundException을 발생시킵니다.
+        User user = userRepository.findByUsername(username)
+            // TODO: [7] 조회된 사용자 정보가 없으면 UsernameNotFoundException을 발생시킵니다.
+            .orElseThrow(() -> new UsernameNotFoundException(username));
         // TODO: [8] 조회된 사용자 정보를 기반으로 UserDetails 객체를 생성하여 반환합니다.
         //      - UserDetails는 Spring Security에서 사용자의 정보를 나타내는 인터페이스입니다.
         //      - org.springframework.security.core.userdetails.User 클래스를 사용하여 UserDetails 객체를 생성할 수 있습니다.
-        return null;
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                Collection<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add((GrantedAuthority) user::getRole);
+                return authorities;
+            }
+
+            @Override
+            public String getPassword() {
+                return user.getPassword();
+            }
+
+            @Override
+            public String getUsername() {
+                return user.getUsername();
+            }
+        };
     }
-} 
+}
