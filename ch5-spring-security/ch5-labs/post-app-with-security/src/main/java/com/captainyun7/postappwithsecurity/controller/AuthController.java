c6c27ed7d32b1b18f73d3b3dffdfa3f2896b015b
namung08 <namung08@gmail.com>
2025-06-24 16:25:50 +0900

    ✨ feat: 사용자 등록 API 추가 - AuthController에 /signup 엔드포인트 및 요청/응답 처리 로직 구현

--- ch5-spring-security/ch5-labs/post-app-with-security/src/main/java/com/captainyun7/postappwithsecurity/controller/AuthController.java
+++ ch5-spring-security/ch5-labs/post-app-with-security/src/main/java/com/captainyun7/postappwithsecurity/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.captainyun7.postappwithsecurity.controller;
 
+import com.captainyun7.postappwithsecurity.dto.user.request.RegisterRequest;
+import com.captainyun7.postappwithsecurity.dto.user.response.UserResponse;
 import com.captainyun7.postappwithsecurity.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
  private final AuthService service;
+
+ @PostMapping("/signup")
+ public ResponseEntity<UserResponse> post(@RequestBody RegisterRequest registerRequest) {
+  return ResponseEntity.status(HttpStatus.CREATED).body(service.register(registerRequest));
+ }
 }
