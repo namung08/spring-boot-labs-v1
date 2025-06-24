6e8fc0d803ae5b5e85f32fdfb6fb7416fc7a20d3
namung08 <namung08@gmail.com>
2025-06-24 11:23:20 +0900

    ✨ feat: UserDetailsServiceImpl에서 UserDetails 객체 생성 로직 개선 - 사용자 권한을 SimpleGrantedAuthority로 설정하고, UserDetails 구현 방식을 변경하여 코드 간결화

--- ch5-spring-security/ch5-self/ch5-03-session-based-spring-security-login/src/main/java/com/captainyun7/ch503sessionbasedspringsecuritylogin/service/UserDetailsServiceImpl.java
+++ ch5-spring-security/ch5-self/ch5-03-session-based-spring-security-login/src/main/java/com/captainyun7/ch503sessionbasedspringsecuritylogin/service/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import com.captainyun7.ch503sessionbasedspringsecuritylogin.domain.User;
 import com.captainyun7.ch503sessionbasedspringsecuritylogin.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -27,23 +29,28 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         // TODO: [8] 조회된 사용자 정보를 기반으로 UserDetails 객체를 생성하여 반환합니다.
         //      - UserDetails는 Spring Security에서 사용자의 정보를 나타내는 인터페이스입니다.
         //      - org.springframework.security.core.userdetails.User 클래스를 사용하여 UserDetails 객체를 생성할 수 있습니다.
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                Collection<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add((GrantedAuthority) user::getRole);
-                return authorities;
-            }
-
-            @Override
-            public String getPassword() {
-                return user.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return user.getUsername();
-            }
-        };
+        return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+//        return new UserDetails() {
+//            @Override
+//            public Collection<? extends GrantedAuthority> getAuthorities() {
+//                Collection<GrantedAuthority> authorities = new ArrayList<>();
+//                authorities.add((GrantedAuthority) () -> "ROLE_" + user.getRole());
+//                return authorities;
+//            }
+//
+//            @Override
+//            public String getPassword() {
+//                return user.getPassword();
+//            }
+//
+//            @Override
+//            public String getUsername() {
+//                return user.getUsername();
+//            }
+//        };
     }
 }
